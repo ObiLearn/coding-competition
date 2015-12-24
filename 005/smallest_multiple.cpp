c69e909b68775859d7cf30ea970589de8c114958
Jan Christoph Uhde <Jan@UhdeJC.com>
2015-12-24 09:31:13 +0100

    add: solution for 005

---
+++ 005/smallest_multiple.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <obi/math/basic_functions.hpp>
+#include <obi/util/show.hpp>
+#include <obi/algorithm.hpp>
+
+int main(int argc, const char *argv[]) {
+    using obi::util::operator<<;
+    namespace bmp = boost::multiprecision;
+    std::vector<std::vector<int>> rv;
+
+    std::map<int,int> factor_map;
+    for(int i = 1; i <= 20; i++) {
+        auto factor_vector = obi::math::prime_factors_naive(i);
+        auto occurrence_map = obi::algorithm::count_occurrences(factor_vector);
+        obi::algorithm::merge_maps(occurrence_map, factor_map, std::less<>());
+    }
+
+    bmp::uint1024_t result = 1;
+    for(auto& mpair : factor_map) {
+        result = result * bmp::uint1024_t(std::pow(mpair.first,mpair.second));
+    }
+
+    std::cout << factor_map << std::endl;
+    std::cout << "result: " << result << std::endl;
+    return 0;
+}
