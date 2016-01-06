8dc16d4af77f2b22fbe8b5fd542dbb813d608475
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-06 07:49:36 +0100

    add: 004 to new makefile

--- 003-largest_prime_factor.cpp
+++ 003-largest_prime_factor.cpp
@@ -3,23 +3,25 @@
 #include <obi/util/show.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
-#ifndef OBI_RUN_MAIN
-#include <gtest/gtest.h>
-TEST(e003_largest_prime_factor, 600851475143ULL){
-    namespace bmp = boost::multiprecision;
-    bmp::uint1024_t n = 600851475143ULL;
-    auto result = obi::math::prime_factors_naive(n,true);
-    EXPECT_EQ(result.back(),6857);
-}
-
-#else
-int main(int argc, const char *argv[]) {
+auto euler_003(){
     namespace bmp = boost::multiprecision;
     bmp::uint1024_t n = 600851475143ULL;
     //bmp::uint1024_t n = 13ULL;
     auto result = obi::math::prime_factors_naive(n,true);
+    return result;
+}
+
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(e003_largest_prime_factor, 600851475143ULL){
+    EXPECT_EQ(euler_003().back(),6857);
+}
+
+#else
+int main(int argc, const char *argv[]) {
 
     using namespace obi::util;
+    auto result = run_euler();
     std::cout  << "factorization " << result << std::endl
                << "result: " << result.back() << std::endl;
 
---
+++ 004-largest_palindrome_product.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <obi/math/basic_functions.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+template <typename T>
+void num_out(T x, T base = T(10)) {
+    std::cout << std::boolalpha << x << ": " << obi::math::is_palindrome_number(x,base) << std::endl;
+}
+
+auto euler_004(bool in_main = false){
+    unsigned int biggest = 0;
+
+    for(unsigned int i = 999; i >= 100; i--){
+        for(unsigned int j = 999; j >= 100; j--){
+            unsigned product = i * j;
+            if(obi::math::is_palindrome_number(product) && (product > biggest)) {
+                biggest = product;
+                if(in_main) {
+                    std::cout << i << " * " << j << " = " << product << std::endl;
+                }
+            }
+        }
+    }
+    return biggest;
+}
+
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(e004_largest_palindrome_product, 3_digit_numbers){
+    EXPECT_EQ(euler_004(),906609);
+}
+
+#else
+int main(int argc, const char *argv[]) {
+    namespace bmp = boost::multiprecision;
+    //bmp::uint1024_t x = 600851475143ULL;
+    //bmp::uint1024_t n = 13ULL;
+    num_out(1234);
+    num_out(123321);
+    num_out(1234321);
+    num_out(110011);
+
+    euler_004(true);
+
+    return 0;
+}
+#endif
