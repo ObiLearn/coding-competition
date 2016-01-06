985c60fc05ef9f349569a32cb2a2ec139ee5d238
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-06 01:00:59 +0100

    setup googletest and add first task (003)

--- 003-largest_prime_factor.cpp
+++ 003-largest_prime_factor.cpp
@@ -3,6 +3,16 @@
 #include <obi/util/show.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(e003_largest_prime_factor, 600851475143ULL){
+    namespace bmp = boost::multiprecision;
+    bmp::uint1024_t n = 600851475143ULL;
+    auto result = obi::math::prime_factors_naive(n,true);
+    EXPECT_EQ(result.back(),6857);
+}
+
+#else
 int main(int argc, const char *argv[]) {
     namespace bmp = boost::multiprecision;
     bmp::uint1024_t n = 600851475143ULL;
@@ -16,3 +26,4 @@ int main(int argc, const char *argv[]) {
     std::cout << std::endl;
     return 0;
 }
+#endif
