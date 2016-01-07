b6f83aba2a5401df8b05a3ebf95364181eefa6cc
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-07 18:23:55 +0100

    add: 007 to googletest

--- 007-10001st_prime.cpp
+++ 007-10001st_prime.cpp
@@ -7,18 +7,32 @@ void show_prime(const auto& prime){
     std::cout << "prime " << prime.n << " : " << prime.value << std::endl;
 }
 
-int main(int argc, const char *argv[]) {
-
+auto euler_007(bool debug = false) {
     auto sieve = obi::math::create_sieve_of_eratosthenes(40000000);
     auto prime = find_nth_prime(1,sieve);
-    show_prime(prime);
-    for(std::size_t i = 1; i < 20; i++){
-        auto next = obi::math::find_next_prime(prime, sieve);
-        show_prime(next);
-        prime = next;
+    if(debug){
+        for(std::size_t i = 1; i < 20; i++){
+            auto next = obi::math::find_next_prime(prime, sieve);
+            show_prime(next);
+            prime = next;
+        }
     }
 
-    show_prime(obi::math::find_nth_prime(10001, sieve));
+    auto result = obi::math::find_nth_prime(10001, sieve);
+    if(debug){
+        show_prime(result);
+    }
+    return result;
+}
 
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(solution, 0007_10001st_prime){
+    EXPECT_EQ(euler_007().value,104743);
+}
+#else
+int main(int argc, const char *argv[]) {
+    euler_007(true)
     return 0;
 }
+#endif
