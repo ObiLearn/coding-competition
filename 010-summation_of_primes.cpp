e0778837853dc16b23d6a02ffcaede704d7d42e2
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-08 01:43:19 +0100

    add: 010 to googletest

--- 010-summation_of_primes.cpp
+++ 010-summation_of_primes.cpp
@@ -4,13 +4,20 @@
 #include <obi/math/primes.hpp>
 #include <obi/util/scoped_timer.hpp>
 
-int main(int argc, const char *argv[]) {
+std::size_t euler_0010(bool debug = false) {
     obi::util::scoped_timer timer;
-    timer.add_step("main begin");
+
+    if (debug) {
+        timer.add_step("main begin");
+    } else {
+        timer.disable();
+    }
 
     std::size_t max = 2 * 1000 * 1000;
     auto sieve = obi::math::create_sieve_of_eratosthenes(max);
-    timer.add_step("sieve created");
+    if (debug) {
+        timer.add_step("sieve created");
+    }
 
     auto prime = find_nth_prime(1,sieve);
     std::size_t prime_sum = 0;
@@ -24,8 +31,23 @@ int main(int argc, const char *argv[]) {
             break;
         }
     }
-    timer.add_step("sum_calculated");
 
-    std::cout << "result: " << prime_sum << std::endl;
+    if(debug) {
+        timer.add_step("sum_calculated");
+        std::cout << "result: " << prime_sum << std::endl;
+    }
+
+    return prime_sum;
+}
+
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(solution, 0010_special_pythagorean_triplet){
+    EXPECT_EQ(euler_0010(), 142913828922);
+}
+#else
+int main(int argc, const char *argv[]) {
+    euler_0010(true)
     return 0;
 }
+#endif
