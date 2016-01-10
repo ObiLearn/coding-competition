55d33d7ab5aaefab74c4b6d039a8bf2463c31c1d
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-10 18:38:10 +0100

    add: 016 to googletest

--- 015-lattice_path.cpp
+++ 015-lattice_path.cpp
@@ -3,8 +3,9 @@
 #include <obi/math/basic_functions.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
-int main(int argc, const char *argv[]) {
-    namespace bmp = boost::multiprecision;
+namespace bmp = boost::multiprecision;
+
+bmp::uint1024_t euler_0015(bool debug = false) {
     namespace om = obi::math;
 
     bmp::uint1024_t n = 40;
@@ -16,10 +17,24 @@ int main(int argc, const char *argv[]) {
 
     auto result = fn / (fk * fnk);
 
-    std::cout << "fn: " << fn << std::endl;
-    std::cout << "fk: " << fk << std::endl;
-    std::cout << "result: " << result << std::endl;
-    std::cout << "result: " << om::binomial_coefficient(n,k) << std::endl;
+    if (debug) {
+        std::cout << "fn: " << fn << std::endl;
+        std::cout << "fk: " << fk << std::endl;
+        std::cout << "result: " << result << std::endl;
+        std::cout << "result: " << om::binomial_coefficient(n,k) << std::endl;
+    }
 
+    return result;
+}
+
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(solution, 0015_large_sum){
+    EXPECT_EQ(euler_0015().str(), "137846528820");
+}
+#else
+int main(int argc, const char *argv[]) {
+    euler_0015(true)
     return 0;
 }
+#endif
