b2b4237310dca9e7b9f83db733adb38663fb456f
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-01-11 00:41:18 +0100

    add: 016 to google test

--- 016-power_digit_sum.cpp
+++ 016-power_digit_sum.cpp
@@ -1,11 +1,12 @@
-// Copyright - 2016 - Jan Christoph Uhde <Jan@UhdeJC.com>
+// Copyright - 20016 - Jan Christoph Uhde <Jan@UhdeJC.com>
 #include <iostream>
 #include <string>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <obi/util/scoped_timer.hpp>
 
-int main(int argc, const char *argv[]) {
-    namespace bmp = boost::multiprecision;
+namespace bmp = boost::multiprecision;
+
+bmp::uint1024_t euler_0016(bool debug = false) {
 
     obi::util::scoped_timer timer;
     bmp::uint1024_t num = bmp::pow(bmp::uint1024_t(2),1000);
@@ -18,9 +19,25 @@ int main(int argc, const char *argv[]) {
     }
     sum += num;
 
-    timer.add_step("solution found");
-    std::cout << "result: " << sum << std::endl;
-    timer.add_step("result printed");
+    if (debug) {
+        timer.add_step("solution found");
+        std::cout << "result: " << sum << std::endl;
+        timer.add_step("result printed");
+    } else {
+        timer.disable();
+    }
 
+    return sum;
+}
+
+#ifndef OBI_RUN_MAIN
+#include <gtest/gtest.h>
+TEST(solution, 0016_large_sum){
+    EXPECT_EQ(euler_0016().str(), "1366");
+}
+#else
+int main(int argc, const char *argv[]) {
+    euler_0016(true)
     return 0;
 }
+#endif
